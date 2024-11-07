d742eafb82f480aa87c2dafad79fd7095f19577a
IGaganpreetSingh <gaganpannu83@gmail.com>
2024-11-07 17:19:05 +0530

    fixes for My Lady also to M'Lady
    Signed-off-by: IGaganpreetSingh <gaganpannu83@gmail.com>

--- helpers.py
+++ helpers.py
@@ -372,6 +372,7 @@ def format_transcript(text):
         line = re.sub(r"\.\.\.", "", line)
         line = re.sub(r'(\d{1,3}(,\d{3})*(\.\d+)?)[ ]+(?<!\w)rand\b(?!\w)', r'R\1', line, flags=re.IGNORECASE)
         line = re.sub(r"\b(my|My)\s+lord\b", r"M'Lord", line, flags=re.IGNORECASE)
+        line = re.sub(r"\b(my|My)\s+lady\b", r"M'Lady", line, flags=re.IGNORECASE)
         # Capitalize the first letter of the sentence after a speaker label
         line = re.sub(
             r"(^Speaker \d+:)\s*(\w)",
